fbbdabd82cd32f7759f151fa40b13fcc64a48045
Justus <66597071+kidshu@users.noreply.github.com>
2022-03-26 12:14:53 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const client = require('twilio')(accountSid, authToken);
 client.notify.services(notifyServiceSid)
   .notifications.create({
     toBinding: JSON.stringify([
-      binding_type: 'sms', address: '** First phone number here **',
-      binding_type: 'sms', address: '** Second phone number here **',
+      binding_type: 'sms', address: '** 15752689458 **',
+      binding_type: 'sms', address: '** 17205255575 **',
       // This also works for iOS, Android, and FB Messenger.
       // You can mix and match binding_type in the toBinding.
     ]),
-    body: 'You just sent your first message with the Passthrough API!'
+    body: 'Justus just sent you a message through Twilio!'
   })
   .then(notification => console.log(notification.sid))
   .catch(error => console.log(error));
